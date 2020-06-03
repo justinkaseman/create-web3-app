0816ef89d601fd9957f9ce0c8b0db4baafa41eb7
Justin Kaseman <justinkaseman@live.com>
2020-06-03 13:33:08 -0700

    feat: basic yargs setup for cli

--- packages/core/src/cli/index.ts
+++ packages/core/src/cli/index.ts
@@ -1,5 +1,26 @@
-import clone from "@create-web3-app/clone";
+import clone from "../../../clone";
+import * as yargs from "yargs";
+
 export default () => {
-  clone();
   console.log(process.version);
+
+  yargs
+    .scriptName("create-web3-app")
+    .usage("$0 <cmd> [args]")
+    .command(
+      "clone",
+      "Start a local cloned mainnet blockchain instance",
+      (yargs) => {
+        yargs.positional("name", {
+          type: "string",
+          default: "Cambi",
+          describe: "the name to say hello to",
+        });
+      },
+      function (argv) {
+        console.log("Starting clone...");
+        clone();
+      }
+    )
+    .help().argv;
 };
