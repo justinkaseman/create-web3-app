741c2df6b7a9a63744fa91f5707d6ea2234f3a7b
Justin Kaseman <justinkaseman@live.com>
2020-06-17 12:17:38 -0700

    feat: Clone attempts to run contract deployments on refresh if using Truffle or Buidler

---
+++ packages/clone/src/config.ts
@@ -0,0 +1,14 @@
+const findUp = require("find-up");
+
+export const getConfig = async (name) => {
+  return await findUp(name);
+};
+
+export const loadConfig = async (path) => {
+  try {
+    const file = require(path);
+    return file;
+  } catch (e) {
+    return false;
+  }
+};
