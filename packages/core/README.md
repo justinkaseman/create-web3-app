89f264b02f63b949417afb576b7a74f4f86d54fe
Justin Kaseman <justinkaseman@live.com>
2020-06-21 22:18:54 -0700

    feat: adds CLI arg documentation to core & a title log

--- packages/core/README.md
+++ packages/core/README.md
@@ -1,3 +1,1 @@
 ### Core
-
-// Turn into only CLI?
--- packages/core/src/cli/index.ts
+++ packages/core/src/cli/index.ts
@@ -13,21 +13,24 @@ export default () => {
       "Start a local cloned mainnet blockchain instance",
       (yargs) => {
         yargs.positional("run", {
+          alias: ["r"],
           type: "string",
-          describe: "where to run your development blockchain",
+          choices: ["process", "docker"],
+          description: "the place to start the development blockchain",
         });
         yargs.positional("node", {
+          alias: ["n"],
           type: "string",
-          describe: "what type of node are you referencing mainnet from",
+          choices: ["full", "archive"],
+          description: "the type of node to clone from",
         });
         yargs.positional("url", {
           type: "string",
-          describe: "the place to access your node",
+          description: "the location of node to clone from",
         });
       },
       function (argv) {
         logTitle();
-        console.log("\nStarting clone...\n");
         clone(argv);
       }
     )
@@ -35,29 +38,34 @@ export default () => {
       "initialize",
       "Start a DApp from using a template architecture",
       (yargs) => {
-        yargs.positional("test", {
+        yargs.positional("template", {
+          alias: ["t"],
           type: "string",
-          default: "Cambi",
-          describe: "the name to say hello to",
+          choices: ["truffle", "buidler"],
+          description: "the code architecture to start from",
+        });
+        yargs.positional("pkgm", {
+          type: "string",
+          choices: ["yarn", "npm"],
+          default: "yarn",
+          description: "the package manager to install dependencies with",
+        });
+        yargs.positional("force", {
+          alias: ["f"],
+          type: "boolean",
+          default: false,
+          description: "copy template into a directory that already has files",
+        });
+        yargs.positional("verbose", {
+          alias: ["v"],
+          type: "boolean",
+          default: false,
+          description: "prints all logs to the console",
         });
       },
       function (argv) {
         logTitle();
-        console.log("\nCreating project from template...\n");
-        const unboxOptions = { force: false };
-
-        // .unbox() validates & unboxes truffle box repos
-        // pass the current working directory as directory to unbox into
-        initializer.unbox(
-          "https://github.com/justinkaseman/create-web3-app",
-          process.cwd(),
-          unboxOptions,
-          {
-            events: {
-              emit: (input) => console.log(input),
-            },
-          }
-        );
+        initializer(argv);
       }
     )
     .help().argv;
