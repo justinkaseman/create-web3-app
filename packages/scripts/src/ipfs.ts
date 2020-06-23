61af0394552d4554956cc963b14724b57a5ca948
Justin Kaseman <justinkaseman@live.com>
2020-06-23 12:55:55 -0700

    feat: scripts - uses yargs to run specific script

--- packages/scripts/src/ipfs.ts
+++ packages/scripts/src/ipfs.ts
@@ -1,16 +1,15 @@
-console.log("#keep");
-// const ipfs = require("ipfs");
-// const IpfsHttpClient = require("ipfs-http-client");
-// const { globSource } = IpfsHttpClient;
+const ipfs = require("ipfs");
+const IpfsHttpClient = require("ipfs-http-client");
+const { globSource } = IpfsHttpClient;
 
-// async function main() {
-//   const IPFS = await ipfs.create();
-//   await IPFS.start();
-//   for await (const file of IPFS.add(
-//     globSource("./build", { recursive: true }),
-//     { progress: (data) => console.log("progress", data) }
-//   )) {
-//     console.log(file);
-//   }
-// }
-// main();
+async function main() {
+  const IPFS = await ipfs.create();
+  await IPFS.start();
+  for await (const file of IPFS.add(
+    globSource("./build", { recursive: true }),
+    { progress: (data) => console.log("progress", data) }
+  )) {
+    console.log(file);
+  }
+}
+main();
